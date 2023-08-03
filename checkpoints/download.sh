beca020154864bbc342ef7f1652ca5f5ea4a7554
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-08-03 12:06:13 -0400

    Downloads source model weights and ignores them in version control

--- checkpoints/download.sh
+++ checkpoints/download.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-gdown "https://drive.google.com/uc?id=1407oe8AooagrHNRZNLrbvFxzM_z2D1Wg"
+# Download and unzip
+gdown "https://drive.google.com/uc?id=1407oe8AooagrHNRZNLrbvFxzM_z2D1Wg"  # Note gdown is a pip package
 unzip pretrained_models.zip
+
+# Cleanup
+mv ./pretrained_models/* .
+mv moco_v1_200ep_pretrain.pth.tar moco_v1_200ep_pretrain.pth
+rm pretrained_models.zip
+rmdir pretrained_models
